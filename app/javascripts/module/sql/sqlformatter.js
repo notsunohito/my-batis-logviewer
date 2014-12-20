69a64157c2fd8104c3331d911f82b3493ebaed92
Shuichiro Kamiya <notsunohito@gmail.com>
2014-12-20 17:20:51 +0900

    Refactor sqlformatter [wip]

--- app/javascripts/module/sql/sqlformatter.js
+++ app/javascripts/module/sql/sqlformatter.js
@@ -6,32 +6,33 @@ var rule = new asf.anbtSqlFormatter.Rule();
 var anbtSqlFormatter = new asf.anbtSqlFormatter.Formatter(rule);
 
 var SqlFormatter = function() { 
+    var patternIN = ' IN \\([^\\)]+\\)';
 
     this.format = function format( unformatted ) {
-        var regex =  / IN \([^\)]+\)/;
+        var regexIN =  new RegExp( patternIN, 'i');
         var anbtFormatted =  anbtSqlFormatter.format( unformatted );
 
-        if(!regex.test(anbtFormatted)) {
+        if(!regexIN.test(anbtFormatted)) {
             return anbtFormatted;
         }
 
         var splitted = splitAtInOperator( anbtFormatted );
-        var result = _.map(splitted, function( element ) {
-            if(regex.test(element)) {
+        var removed = _.map(splitted, function( element ) {
+            if(regexIN.test(element)) {
                 return removeCRLF(element);
             }
             return element;
         }).join('');
-        return result;
+        return removed;
     };
 
     function splitAtInOperator( anbtFormatted ) {
-        var regex =  / IN \([^\)]+\)/g;
+        var regexIN =  new RegExp( patternIN, 'ig');
         var match = [];
         var result = [];
         var prevMatchedTailIndex = 0;
         var matchedLength = 0;
-        while(match = regex.exec( anbtFormatted )) {
+        while(match = regexIN.exec( anbtFormatted )) {
             var notMatched = anbtFormatted.substring( prevMatchedTailIndex + matchedLength, match.index);
             prevMatchedTailIndex = match.index;
             matchedLength = match[0].length;
@@ -42,7 +43,7 @@ var SqlFormatter = function() {
     }
 
     function removeCRLF( matched ) {
-        var result = matched.replace(/\n/g,'');
+        var result = matched.replace(/\r\n|\r|\n/g,'');
         result = result.replace(/ +(?=[,\)])/g,'');
         result = result.replace(/ +/g,' ');
         return result;
