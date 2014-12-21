4b5711643a7ce6509e0a04d419a52f6604dbc261
Shuichiro Kamiya <notsunohito@gmail.com>
2014-12-21 23:02:28 +0900

    Add Application#clear, Application#paste

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,24 +1,38 @@
-var _ = require('underscore');
-var $ = require('jquery');
-var Backbone = require('backbone');
+var _              = require('underscore');
+var $              = require('jquery');
+var Backbone       = require('backbone');
 var QueryExtractor = require('./module/log/queryextractor.js');
-var QueriesView = require('./query/queriesview.js');
+var QueriesView    = require('./query/queriesview.js');
 
 var Application = {
     initialize: function() {
         $('#loginput').on('input', this.display);
+        $('#clear').on('click', this.clear);
+        $('#paste').on('click', this.paste);
     },
     display: function() {
-        var text = $('#loginput')[0].value;
-        if(!text) return;
-        var queries = QueryExtractor.extract( text  );
+        var input = $('#loginput')[0].value;
+        var queries = QueryExtractor.extract( input );
         Application.render( queries );
     },
-    render: function(qArr) {
-        var models = _.map(qArr, function(query) { return new Backbone.Model( query ); });
-        var queries = new Backbone.Collection( models );
-        var queriesView = new QueriesView({collection: queries });
+    render: function(queries) {
+        var queryModels = _.map(queries, function(query) {
+            return new Backbone.Model( query ); 
+        });
+        var queryCollection = new Backbone.Collection( queryModels );
+        var queriesView = new QueriesView({ collection: queryCollection });
         $('#main').html( queriesView.render().el );
+    },
+    clear: function() {
+        $('#loginput')[0].value = '';
+        $('#main').empty();
+    },
+    paste: function() {
+        if (window.clipboardData){
+            var clipboardData = window.clipboardData.getData( "Text" );
+            $('#loginput')[0].value = clipboardData;
+            $('#loginput').trigger("input");
+        }
     }
 };
 
